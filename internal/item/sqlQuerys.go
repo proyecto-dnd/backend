2ee9ae7fe3414a4e32610094bb66682fa87d42aa
IvanTarjan <ivantarjan2003@gmail.com>
2024-02-28 19:57:51 -0300

    Completing item, weapon and their relationships' queries

--- internal/item/sqlQuerys.go
+++ internal/item/sqlQuerys.go
@@ -1,10 +1,10 @@
 package item
 
 var (
-	QueryCreateItem = ``
-    QueryGetAll = ``
-	QueryGetByCampaignId = ``
-    QueryGetById = ``
-    QueryUpdate = ``
-    QueryDelete = ``
+	QueryCreateItem = `INSERT INTO item (name, weight, price, description, campaign_id) VALUES (?, ?, ?, ?, ?);`
+    QueryGetAll = `SELECT * FROM item;`
+	QueryGetByCampaignId = `SELECT * FROM item WHERE campaign_id = ?;`
+    QueryGetById = `SELECT * FROM item WHERE item_id = ?;`
+    QueryUpdate = `UPDATE item SET name = ?, weight = ? , price = ? , description = ? , campaign_id = ? WHERE item_id = ?;`
+    QueryDelete = `DELETE from item where item_id = ?;`
 )
--- internal/itemXCharacterData/sqlQuerys.go
+++ internal/itemXCharacterData/sqlQuerys.go
@@ -1,11 +1,11 @@
 package itemxcharacterdata
 
 var (
-	QueryCreateItemXCharacterData = ``
-    QueryGetAll = ``
-    QueryGetById = ``
-    QueryGetByCharacterDataId = ``
-    QueryUpdate= ``
-    QueryDelete= ``
-    QueryDeleteByCharacterDataId = ``
+	QueryCreateItemXCharacterData = `INSERT INTO character_item (character_id, item_id, quantity) values (?, ?, ?)`
+    QueryGetAll = `SELECT character_item_id, character_id, item.item_id , name, weight, price, description, campaign_id, quantity FROM sql10684003.character_item LEFT JOIN item ON character_item.item_id = item.item_id;`
+    QueryGetById = `SELECT character_item_id, character_id, item.item_id , name, weight, price, description, campaign_id, quantity FROM sql10684003.character_item LEFT JOIN item ON character_item.item_id = item.item_id WHERE character_item_id = ?;`
+    QueryGetByCharacterDataId = `SELECT character_item_id, character_id, item.item_id , name, weight, price, description, campaign_id, quantity FROM sql10684003.character_item LEFT JOIN item ON character_item.item_id = item.item_id WHERE character_id = ?;`
+    QueryUpdate= `UPDATE character_item SET character_id = ? , item_id = ? , quantity = ? WHERE character_item_id = ?`
+    QueryDelete= `DELETE FROM character_item WHERE character_item_id = ?;`
+    QueryDeleteByCharacterDataId = `DELETE FROM character_item where character_id = ?;`
 )
