7b558cb7a492ae8925a208fa1869e36d9d2c9260
alvarezgonzalo0022 <alvarezgonzalo0022@gmail.com>
2024-02-28 20:38:00 -0300

    Adding character_feature and event_type CRUD

---
+++ internal/character_feature/interface.go
@@ -0,0 +1,31 @@
+package character_feature
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/proyecto-dnd/backend/internal/domain"
+	"github.com/proyecto-dnd/backend/internal/dto"
+)
+
+type CharacterFeatureHandler interface {
+	HandlerCreate() gin.HandlerFunc
+	HandlerGetAll() gin.HandlerFunc
+	HandlerGetByFeatureId() gin.HandlerFunc
+	HandlerGetByCharacterId() gin.HandlerFunc
+	HandlerDelete() gin.HandlerFunc
+}
+
+type CharacterFeatureService interface {
+	CreateCharacterFeature(dto.CreateCharacterFeatureDto) (domain.CharacterFeature, error)
+	GetAllCharacterFeatures() ([]domain.CharacterFeature, error)
+	GetCharacterFeatureByFeatureId(id int) ([]domain.CharacterFeature, error)
+	GetCharacterFeatureByCharacterId(id int) ([]domain.CharacterFeature, error)
+	DeleteCharacterFeature(idFeature int, idCharacter int) error
+}
+
+type CharacterFeatureRepository interface {
+	Create(CharacterFeature domain.CharacterFeature) (domain.CharacterFeature, error)
+	GetAll()([]domain.CharacterFeature, error)
+	GetByFeatureId(id int)([]domain.CharacterFeature, error)
+	GetByCharacterId(id int)([]domain.CharacterFeature, error)
+	Delete(idFeature int, idCharacter int)error
+}
