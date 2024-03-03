fb097535c8b31247f3dee6d68788e1b3967ef21e
javier-a-paredes <javi4202@gmail.com>
2024-03-03 19:01:03 -0300

    changing data types for saving throws properties (int to bool)

--- internal/domain/savingThrows.go
+++ internal/domain/savingThrows.go
@@ -1,12 +1,12 @@
 package domain
 
 type SavingThrow struct {
-	SavingThrowId int `json:"saving_thow_id"`
-	ClassId       int `json:"class_id"`
-	Str           int `json:"str"`
-	Dex           int `json:"dex"`
-	Int           int `json:"int"`
-	Con           int `json:"con"`
-	Wiz           int `json:"wiz"`
-	Cha           int `json:"cha"`
+	SavingThrowId int  `json:"saving_thow_id"`
+	ClassId       int  `json:"class_id"`
+	Str           bool `json:"str"`
+	Dex           bool `json:"dex"`
+	Int           bool `json:"int"`
+	Con           bool `json:"con"`
+	Wiz           bool `json:"wiz"`
+	Cha           bool `json:"cha"`
 }
--- internal/dto/savingThrow.go
+++ internal/dto/savingThrow.go
@@ -1,11 +1,11 @@
 package dto
 
 type SavingThrowDto struct {
-	ClassId int `json:"class_id"`
-	Str     int `json:"str"`
-	Dex     int `json:"dex"`
-	Int     int `json:"int"`
-	Con     int `json:"con"`
-	Wiz     int `json:"wiz"`
-	Cha     int `json:"cha"`
+	ClassId int  `json:"class_id"`
+	Str     bool `json:"str"`
+	Dex     bool `json:"dex"`
+	Int     bool `json:"int"`
+	Con     bool `json:"con"`
+	Wiz     bool `json:"wiz"`
+	Cha     bool `json:"cha"`
 }
