3b063df6598a114692f35205c284dafb253d1ceb
alvarezgonzalo0022 <alvarezgonzalo0022@gmail.com>
2024-03-15 17:52:36 -0300

    Adding fix on event relations

--- internal/domain/event.go
+++ internal/domain/event.go
@@ -4,8 +4,8 @@ type Event struct {
 	EventId            int    `json:"event_id"`
 	Type               int    `json:"type"`
 	Environment        string `json:"environment"`
-	Session_id         int    `json:"session_id"`
-	EventProtagonistId int    `json:"event_protagonist_id"`
+	Session_id         *int    `json:"session_id"`
+	EventProtagonistId *int    `json:"event_protagonist_id"`
 	Dice_rolled        bool   `json:"dice_rolled"`
 	Difficulty_Class   bool   `json:"difficulty_class"`
 	EventTarget        string `json:"event_target"`
