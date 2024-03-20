cf59ce09786923f3a114eb53c9b200f637ece0d9
javier-a-paredes <javi4202@gmail.com>
2024-03-19 21:03:30 -0300

    prueba binario + dockerfile

---
+++ internal/dto/CharacterCardDto.go
@@ -0,0 +1,11 @@
+package dto
+
+type CharacterCardDto struct {
+	CharacterId int     `json:"character_id"`
+	CampaignID int     `json:"campaign_id"`	
+	Name        string  `json:"name"`
+	Race        string  `json:"race"`
+	Class       string  `json:"class"`
+	Level       int     `json:"level"`
+	HitPoints   int     `json:"hit_points"`
+}
